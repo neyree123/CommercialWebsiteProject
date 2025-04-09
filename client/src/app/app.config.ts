c6fa945ea8e9b7707deda2554a7eee42d8c43986
Destiny Treptau <54587172+neyree123@users.noreply.github.com>
2025-04-08 22:01:01 -0500

    Setting up more shop features for the products and the shopping cart page

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
+
+function initApp( initService: InitService) {
+  return () => lastValueFrom(initService.initService()).finally(() => {
+    const splash = document.getElementById('initial-splash');
+    if(splash) {
+      splash.remove();
+    }
+  });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
-    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))]
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      multi: true,
+      deps: [InitService]
+    }
+]
 };
