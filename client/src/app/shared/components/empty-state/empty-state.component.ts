5b2fd39cb2dd7b6cf35b8dc170de64bd1cde060d
Destiny Treptau <54587172+neyree123@users.noreply.github.com>
2025-04-13 21:59:23 -0500

    Finishing Azure setup

--- client/src/app/shared/components/empty-state/empty-state.component.ts
+++ client/src/app/shared/components/empty-state/empty-state.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
@@ -14,5 +14,12 @@ import { RouterLink } from '@angular/router';
   styleUrl: './empty-state.component.scss'
 })
 export class EmptyStateComponent {
+  message = input.required<string>();
+  icon = input.required<string>();
+  actionText = input.required<string>();
+  action = output<void>();
 
+  onAction() {
+    this.action.emit();
+  }
 }
